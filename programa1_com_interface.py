62d4e4262bfc0326cfd3c428786675dc4476ee9a
leonardohenri2306 <36735763+leonardohenri2306@users.noreply.github.com>
2018-03-05 12:21:03 -0300

    Update programa1_com_interface.py
    Segunda parte

--- programa1_com_interface.py
+++ programa1_com_interface.py
@@ -1,35 +1,58 @@
+
 from tkinter import *
 
 janela = Tk()
 
 def bt_click():
-    print(ed.get())
+    arquivo = open('dados.txt', 'w')
+    arquivo.write("Full name: %s\n" %(fn.get()))
+    arquivo.write("User name: %s\n" %(un.get()))
+    arquivo.write("individual registration: %s\n" %(cpf.get()))
+    arquivo.write("Date of birth: %s\n" %(db.get()))
+    arquivo.close()
+
+    print(fn.get())
+    print(un.get())
+    print(cpf.get())
+
+
 
 #Nome completo
-ed = Entry(janela)
-ed.place(x= 20, y= 45)
-bt = Button(janela, width=3, text = "Ok", command=bt_click)
-bt.place(x= 190, y= 40)
+fn = Entry(janela, width=50)
+fn.place(x= 20, y= 45)
+btfn = Button(janela, width=3, text = "Ok", command=bt_click)
+btfn.place(x= 430, y= 40)
 
 #Nome de usuario
-ed2 = Entry(janela)
-ed2.place(x= 20, y= 90)
-bt = Button(janela, width=3, text = "Ok", command=bt_click)
-bt.place(x= 190, y= 90)
+un = Entry(janela, width=50)
+un.place(x= 20, y= 90)
+btun = Button(janela, width=3, text = "Ok", command=bt_click)
+btun.place(x= 430, y= 90)
+
+#Data de nascimento
+db = Entry(janela)
+db.place(x= 20, y= 150,)
+btdb = Button(janela, width=3, text = "Ok",)
+btdb.place(x= 190, y=150)
+
+#cpf
+cpf = Entry(janela)
+cpf.place(x= 20, y= 200)
+btun = Button(janela, width=2, text = "Ok", command=bt_click)
+btun.place(x= 190, y=200)
 
 #Titulos e janelas
 janela.title("Register program")
-lb3 = Label(janela, text = "Full name")
-lb3.place(x= 20, y= 25)
+lbfn = Label(janela, text = "Full name")
+lbfn.place(x= 20, y= 25)
 
-lb4 = Label(janela, text = "User name")
-lb4.place(x= 20, y= 69)
+lbun = Label(janela, text = "User name")
+lbun.place(x= 20, y= 69)
 
 lb = Label (janela, text = "Start Register")
 lb.place(x=110, y=5)
 
-janela.geometry("300x300+200+200")
-
+janela.geometry("800x600+300+100")
 
 
 janela.mainloop()
